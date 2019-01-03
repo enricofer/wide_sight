0bd5ddf4588f4936ce1d7980153cc8c345553f10
enrico <enricofer@gmail.com>
2019-01-03 12:49:15 +0100

    filter by type on img_obj rest

--- wide_sight/views.py
+++ wide_sight/views.py
@@ -127,7 +127,7 @@ class image_objectsViewSet(viewsets.ModelViewSet):
     permission_classes = ( Or(baseAPIPermission, IsAuthenticated, HasAPIAccess),)
     pagination_class = basePagination
     filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ('panorama', 'id', )
+    filterset_fields = ('panorama', 'id', 'type')
 
 
 class userkeysViewSet(viewsets.ReadOnlyModelViewSet):
